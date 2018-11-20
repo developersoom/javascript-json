0701cb95afa0ac8cd0bb4275de20cd7f5f533313
developersoom <developer.soom@gmail.com>
2018-11-20 15:24:57 +0900

    Remove 'continue'

--- Lec4_json.js
+++ Lec4_json.js
@@ -10,20 +10,15 @@ function scan(str) {
     let tokens = [];
     let stack = "";
     for (let token of str) {
-        if (token === ' ') {
-            continue;
-        }
-        if (token === ',' || token === '[') {
+        if (token === ' ') {} else if (token === ',' || token === '[') {
             stack === "" ? tokens.push(token) : tokens.push(stack);
             stack = "";
-            continue;
-        }
-        if (token === ']') {
+        } else if (token === ']') {
             tokens.push(stack);
             stack = token;
-            continue;
+        } else {
+            stack += token;
         }
-        stack += token;
     }
     return tokens;
 }
@@ -38,27 +33,19 @@ function parse(str) {
     for (let token of tokens) {
         if (token === '[') {
             result.push(new Data('array', value, child));
-            continue;
-        }
-        if (Number(token)) {
+        } else if (Number(token)) {
             const lastChild = result[result.length - 1].child;
             lastChild.push(new Data('number', token));
             type = "", value = "", child = [];
-            continue;
-        }
-        if (token === 'null') {
+        } else if (token === 'null') {
             const lastChild = result[result.length - 1].child;
             lastChild.push(new Data('null', token));
             type = "", value = "", child = [];
-            continue;
-        }
-        if (token === 'true' || token === 'false') {
+        } else if (token === 'true' || token === 'false') {
             const lastChild = result[result.length - 1].child;
             lastChild.push(new Data('boolean', token));
             type = "", value = "", child = [];
-            continue;
-        }
-        if (token[0] === "'" && token[token.length - 1] === "'") {
+        } else if (token[0] === "'" && token[token.length - 1] === "'") {
             if (!countApostrophe(token)) {
                 console.log(`${token}은 올바른 문자열이 아닙니다.`);
                 return;
@@ -66,16 +53,14 @@ function parse(str) {
             const lastChild = result[result.length - 1].child;
             lastChild.push(new Data('string', token));
             type = "", value = "", child = [];
-            continue;
-        }
-        if (token === ']' && result.length > 1) {
+        } else if (token === ']' && result.length > 1) {
             const lastData = result.pop();
             const lastChild = result[result.length - 1].child;
             lastChild.push(lastData);
-            continue;
+        } else {
+            console.log(`${token}은 올바른 문자열이 아닙니다.`);
+            return;
         }
-        console.log(`${token}은 올바른 문자열이 아닙니다.`);
-        return;
 
     }
     return result;
@@ -89,7 +74,6 @@ function countApostrophe(token) {
     if (count === 2) return true;
     return false;
 }
-
 
 //test
 // var str = "[123,[22,23,[11,[112233],112],55],33]";
