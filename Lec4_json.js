06a944ef2c697f33a41641b7d7e30da140077346
developersoom <developer.soom@gmail.com>
2018-11-21 19:18:02 +0900

    Edit scan() - add condition for checking array inside object

--- Lec4_json.js
+++ Lec4_json.js
@@ -10,13 +10,17 @@ function scan(str) {
     let tokens = [];
     let stack = "";
     for (let token of str) {
-        if (token === ',' || token === '[' || token === '{') {
+        if (stack === ':' && token === '['){
+            tokens.push(stack);
+            tokens.push(token);
+            stack = "";
+        } else if (token === ',' || token === '[' || token === '{') {
             stack === "" ? tokens.push(token) : tokens.push(stack);
             stack = "";
         } else if (token === ']' || token === '}' || token === ':') {
             tokens.push(stack);
             stack = token;
-        } else if (token === ' ') {
+        } else if (token === ' '){
             continue;
         } else {
             stack += token;
