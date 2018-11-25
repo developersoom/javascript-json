543fc8da9053b8277261f1063c2169967973ecff
developersoom <developer.soom@gmail.com>
2018-11-25 16:58:41 +0900

    Edit scan() - refactoring

--- Lec4_json.js
+++ Lec4_json.js
@@ -11,22 +11,19 @@ function scan(str) {
     let tokens = [];
     let stack = "";
     for (let token of str) {
-        if (stack === ':') {
-            tokens.push(stack);
-            stack = "";
-        } else if (token === ',' || token === '[' || token === '{') {
+        if (token === ',' || token === '[' || token === '{') {
             stack === "" ? tokens.push(token) : tokens.push(stack);
             stack = "";
         } else if (token === ']' || token === '}' || token === ':') {
-            tokens.push(stack);
-            stack = token;
+            if(stack !== ""){tokens.push(stack)};
+            tokens.push(token);
+            stack = "";
         } else if (token === ' ') {
             continue;
         } else {
             stack += token;
         }
     }
-//     tokens.push(stack);
     return tokens;
 }
 
