ced4d8def1f75c436d65880611023bf51295b3e9
developersoom <developer.soom@gmail.com>
2018-11-25 17:06:52 +0900

    Edit scan() - divide condition checking comma to avoid it being tokens

--- Lec4_json.js
+++ Lec4_json.js
@@ -6,13 +6,15 @@ class Data {
         this.child = child || undefined;
     }
 }
-
 function scan(str) {
     let tokens = [];
     let stack = "";
     for (let token of str) {
-        if (token === ',' || token === '[' || token === '{') {
+        if (token === '[' || token === '{') {
             stack === "" ? tokens.push(token) : tokens.push(stack);
+            stack = "";
+        } else if(token === ','){
+            if(stack !== "") {tokens.push(stack)};
             stack = "";
         } else if (token === ']' || token === '}' || token === ':') {
             if(stack !== ""){tokens.push(stack)};
