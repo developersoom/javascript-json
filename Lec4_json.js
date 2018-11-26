60813ad08fb49bcf382ae15da8c12685d5339459
developersoom <developer.soom@gmail.com>
2018-11-26 18:35:11 +0900

    Edit parse() - delete useless variables

--- Lec4_json.js
+++ Lec4_json.js
@@ -32,13 +32,13 @@ function scan(str) {
 function parse(str) {
     const tokens = scan(str);
     let result = [];
-    let temp = "", value = "", child = [], objectStatus = false;
+    let temp = "", objectStatus = false;
 
     for (let token of tokens){
         if (token === '[') {
-            result.push(new Data('array', value, child));
+            result.push(new Data('array', "", []));
         } else if (token === '{') {
-            result.push(new Data('object', value, child));
+            result.push(new Data('object', "", []));
             objectStatus = true;
         } else if (token === ':'){
             const lastChild = result[result.length - 1].child;
@@ -46,15 +46,12 @@ function parse(str) {
         } else if (!isNaN(Number(token))) {
             const lastChild = result[result.length - 1].child;
             lastChild.push(new Data('number', token));
-            type = "", value = "", child = [];
         } else if (token === 'null') {
             const lastChild = result[result.length - 1].child;
             lastChild.push(new Data('null', token));
-            type = "", value = "", child = [];
         } else if (token === 'true' || token === 'false') {
             const lastChild = result[result.length - 1].child;
             lastChild.push(new Data('boolean', token));
-            type = "", value = "", child = [];
         } else if (token[0] === "'") {
             if (!countApostrophe(token)) {
                 console.log(`${token}은 올바른 문자열이 아닙니다.`);
@@ -62,7 +59,6 @@ function parse(str) {
             }
             const lastChild = result[result.length - 1].child;
             lastChild.push(new Data('string', token));
-            type = "", value = "", child = [];
         } else if (token === ']' && result.length > 1) {
             const lastData = result.pop();
             const lastChild = result[result.length - 1].child;
@@ -98,4 +94,4 @@ var str = "['1a3',[null,false,['11',[112233],{easy : ['hello', {a: 'a' }, 'world
 // var str = "[1,{a:'str', b:[912,[5656,33]]}]";
 // var str = "[1,{key: [2,{a:'a'}]}]"
 console.log(parse(str))
-// console.log(JSON.stringify(parse(str), null, 2));
+// console.log(JSON.stringify(parse(str), null, 2));ㄴ
